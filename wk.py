a1843a2dc2766e8ac12eb5974efb9bfaa58b299e
阿慈 <136092673+YourAhTzu@users.noreply.github.com>
2023-09-06 15:02:29 +0800

    更新 wk.py

--- wk.py
+++ wk.py
@@ -5,8 +5,8 @@ by 偷CK的六舅哥
 cookies格式 url#cookie#argus#ladon
 wkllq_ck = "url#cookie#argus#ladon"
 7.3 悟空浏览器刷视频，不黑一天4块左右
+9.6 增加变量提交和ua
 暂时未写异常处理,bug提交 https://t.me/jiangyutck
-9.6 增加变量提交
 """
 
 import datetime
@@ -21,7 +21,9 @@ import json
 env_name = 'wk' 
 cookies = os.getenv(env_name)
 
-ua = ""
+
+env_name = 'wkua' 
+ua = os.getenv(env_name)
 
 num=30 #循环参数
 
