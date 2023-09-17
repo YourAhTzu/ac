1f7b3de7d62df2e0593e74b3160affc67f4b8182
阿慈 <136092673+YourAhTzu@users.noreply.github.com>
2023-09-17 18:29:27 +0800

    更新 nbbsp.py
    优化更新

--- nbbsp.py
+++ nbbsp.py
@@ -5,7 +5,6 @@ corn：10 10 * * *
 import os
 import requests
 
-
 # 设置请求头信息
 headers = {
     "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 15_6_1 like Mac OS X) "
@@ -16,27 +15,34 @@ headers = {
 }
 
 
-def send_video_request(video_num, renwu_id, session_id):
-    url = f"https://sc.gdzfxc.com/?s=/ApiSign/videoRenwu&aid=1&platform=wx&session_id={session_id}&pid=0"
-    data = {
-        "renwu_id": renwu_id
-    }
+def send_video_request(account, renwu_ids):
+    url = f"https://sc.gdzfxc.com/?s=/ApiSign/videoRenwu&aid=1&platform=wx&session_id={account}&pid=0"
+    results = []
 
-    try:
-        response = requests.post(url, headers=headers, json=data)
-        response_data = response.json()
+    for i, renwu_id in enumerate(renwu_ids, start=1):
+        data = {
+            "renwu_id": renwu_id
+        }
 
-        status = response_data.get("status")
-        msg = response_data.get("msg")
-        scoreadd = response_data.get("scoreadd")
+        try:
+            response = requests.post(url, headers=headers, json=data)
+            response_data = response.json()
 
-        print(f"第{video_num}个视频：")
-        print(f"请求状态：{status}")
-        print(f"消息：{msg}")
-        print(f"积分：{scoreadd}")
-        print("==============================")
-    except:
-        print(f"视频请求失败")
+            status = response_data.get("status")
+            msg = response_data.get("msg")
+            scoreadd = response_data.get("scoreadd")
+
+            result = {
+                "视频编号": i,
+                "请求状态": status,
+                "消息": msg,
+                "积分": scoreadd
+            }
+            results.append(result)
+        except:
+            print(f"发送第{i}个视频请求失败")
+
+    return results
 
 
 def main():
@@ -47,10 +53,26 @@ def main():
 
     renwu_ids = [1, 2, 3, 10]
     account_list = nbb_accounts.split("\n")
-    
-    for i, renwu_id in enumerate(renwu_ids, start=1):
-        if i <= len(account_list):
-            send_video_request(i, renwu_id, account_list[i-1])
+
+    all_results = []
+    for account in account_list:
+        results = send_video_request(account, renwu_ids)
+        all_results.extend(results)
+
+    for result in all_results:
+        video_num = result["视频编号"]
+        status = result["请求状态"]
+        msg = result["消息"]
+        scoreadd = result["积分"]
+
+        print(f"第{video_num}个视频：")
+        print(f"请求状态：{status}")
+        print(f"消息：{msg}")
+        print(f"积分：{scoreadd}")
+        print("==============================")
+
+    if not all_results:
+        print("未接收到任何视频请求结果")
 
 
 if __name__ == "__main__":
