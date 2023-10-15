9c771920fcf1676eecd47db54f98d9dd3b8abec9
阿慈 <136092673+YourAhTzu@users.noreply.github.com>
2023-10-15 11:38:25 +0800

    更新 cylm.py

--- cylm.py
+++ cylm.py
@@ -1,32 +1,30 @@
-''' 
+'''
 注册链接：https://52.yyyy.run//index/wechat/login/share_id/4063
 抓token环境名字cylm
 new Env('创娱联盟'); 
 '''
 import requests
-import time
 import os
+import time
+import random
+import json
 
-env_name = 'cylm' 
+env_name = 'cylm'
 env = os.getenv(env_name)
 
 url = 'http://52.yyyy.run/api/sign/signTimeEnd'
 headers = {
     'os': 'android',
-    'appid': '',
     'terminal': '2',
     'token': env,
-    'user-agent': 'Mozilla/5.0 (Linux; Android 11; V2068A Build/RP1A.200720.012; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/83.0.4103.106 Mobile Safari/537.36 uni-app Html5Plus/1.0 (Immersed/29.0)',
     'Content-Type': 'application/x-www-form-urlencoded',
     'Accept-Encoding': 'gzip'
 }
-
-task_ids = range(8960, 8964)
-
-for task_id in task_ids:
-    data = {
-        'task_id': str(task_id)
-    }
+for task_id in range(8960, 8964):
+    data = {'task_id': task_id}
     response = requests.post(url, headers=headers, data=data)
-    print(f"请求结果: {response.text}")
-    time.sleep(15) 
+    if response.status_code == 200:
+​        result = response.json()
+        result_json = json.dumps(result, ensure_ascii=False, indent=4)
+    print(result_json)
+    delay = random.randint(15, 20)
