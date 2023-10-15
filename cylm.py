192a00ef03446fea31058aca77ad68552d9d89ac
阿慈 <136092673+YourAhTzu@users.noreply.github.com>
2023-10-15 11:35:15 +0800

    更新 cylm.py

--- cylm.py
+++ cylm.py
@@ -5,7 +5,6 @@ new Env('创娱联盟');
 '''
 import requests
 import time
-import random
 import os
 
 env_name = 'cylm' 
@@ -14,20 +13,20 @@ env = os.getenv(env_name)
 url = 'http://52.yyyy.run/api/sign/signTimeEnd'
 headers = {
     'os': 'android',
+    'appid': '',
     'terminal': '2',
-    'authorization': env,
+    'token': env,
+    'user-agent': 'Mozilla/5.0 (Linux; Android 11; V2068A Build/RP1A.200720.012; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/83.0.4103.106 Mobile Safari/537.36 uni-app Html5Plus/1.0 (Immersed/29.0)',
     'Content-Type': 'application/x-www-form-urlencoded',
     'Accept-Encoding': 'gzip'
 }
 
-for task_id in range(8960, 8964):
-    data = {'task_id': task_id}
+task_ids = range(8960, 8964)
+
+for task_id in task_ids:
+    data = {
+        'task_id': str(task_id)
+    }
     response = requests.post(url, headers=headers, data=data)
-    
-    if response.status_code == 200:
-        result = response.json()
-        msg = result.get('msg', '')
-        print(f'签到结果: {msg}')
-        
-    delay = random.randint(15, 20)
-    time.sleep(delay)
+    print(f"请求结果: {response.text}")
+    time.sleep(15) 
