ac485b0eb26b7ced96d80f5a7416b2549d3d1668
阿慈 <136092673+YourAhTzu@users.noreply.github.com>
2023-10-15 11:47:57 +0800

    更新 cylm.py

--- cylm.py
+++ cylm.py
@@ -5,13 +5,17 @@ new Env('创娱联盟');
 '''
 import requests
 import time
+import os
+
+env_name = 'cylm'
+env = os.getenv(env_name)
 
 url = 'http://52.yyyy.run/api/sign/signTimeEnd'
 headers = {
     'os': 'android',
     'appid': '',
     'terminal': '2',
-    'token': '43099d8e-2529-4cb8-99a8-7d404f951ca2',
+    'token': env,
     'user-agent': 'Mozilla/5.0 (Linux; Android 11; V2068A Build/RP1A.200720.012; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/83.0.4103.106 Mobile Safari/537.36 uni-app Html5Plus/1.0 (Immersed/29.0)',
     'Content-Type': 'application/x-www-form-urlencoded',
     'Accept-Encoding': 'gzip'
