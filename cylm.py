19279ff843b04c84854d7f47a7242fd769a217f7
阿慈 <136092673+YourAhTzu@users.noreply.github.com>
2023-10-15 14:41:51 +0800

    更新 cylm.py
    更新提现

--- cylm.py
+++ cylm.py
@@ -4,8 +4,8 @@
 new Env('创娱联盟'); 
 '''
 import requests
-import time
 import os
+import time
 
 env_name = 'cylm'
 env = os.getenv(env_name)
@@ -13,21 +13,31 @@ env = os.getenv(env_name)
 url = 'http://52.yyyy.run/api/sign/signTimeEnd'
 headers = {
     'os': 'android',
-    'appid': '',
-    'terminal': '2',
     'token': env,
-    'user-agent': 'Mozilla/5.0 (Linux; Android 11; V2068A Build/RP1A.200720.012; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/83.0.4103.106 Mobile Safari/537.36 uni-app Html5Plus/1.0 (Immersed/29.0)',
     'Content-Type': 'application/x-www-form-urlencoded',
-    'Accept-Encoding': 'gzip'
 }
 
 task_ids = range(8960, 8964)
 
-for task_id in task_ids:
-    data = {
-        'task_id': str(task_id)
-    }
-    response = requests.post(url, headers=headers, data=data)
-    # 处理响应结果
-    print(f"Task ID: {task_id}，运行返回：{response.text}")
-    time.sleep(15) 
+with requests.Session() as session:
+    session.headers.update(headers)
+    for task_id in task_ids:
+        data = {
+            'task_id': str(task_id)
+        }
+        response = session.post(url, data=data)
+        print(f"ID: {task_id}，运行返回：{response.text}")
+        time.sleep(15) 
+    
+    
+url = 'http://52.yyyy.run/api/user/postWith'
+data = {'num': '1'}
+headers['Referer'] = 'http://52.yyyy.run/pages/user/myWithdrawal.html'
+headers['Origin'] = 'http://52.yyyy.run'
+
+with requests.Session() as session:
+    session.headers.update(headers)
+    response = session.post(url, data=data)
+    msg = response.json()['msg']
+    result = f"提现结果：{msg}"
+    print(result)
