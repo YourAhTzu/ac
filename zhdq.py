b05f548de6fe2742334665bd903530b9d98e5440
阿慈 <136092673+YourAhTzu@users.noreply.github.com>
2023-10-20 20:47:55 +0800

    更新 zhdq.py
    更新

--- zhdq.py
+++ zhdq.py
@@ -1,9 +1,10 @@
-#抓Authorization填变量zhdq多号@隔开（什么时候签的到怎么定时）链接http://www.mofamoli.com/h5/reg.html?invite_code=JYWG5E
-
+''' 
+抓Authorization填变量zhdq多号@隔开（什么时候签的到怎么定时）链接http://www.mofamoli.com/h5/reg.html?invite_code=JYWG5E
 new Env('智慧地球');
-
+'''
 import os
 import requests
+import datetime
 
 accounts = os.environ.get("zhdq").split("@")
 url = "http://www.zhihuidiqiu.com.cn/api/v2/mining/start"
@@ -11,7 +12,6 @@ headers = {
     "os": "android",
     "appVersionCode": "3",
     "appVersionName": "1.0.2",
-    "datetime": "2023-10-20 18:28:13.700",
     "Accept": "application/json",
     "Local": "1",
     "Content-Length": "0",
@@ -21,9 +21,17 @@ headers = {
     "User-Agent": "okhttp/4.10.0"
 }
 
-for account in accounts:
+for index, account in enumerate(accounts):
+    print(f"执行第{index+1}账号任务")
+    
     auth = f"Bearer {account.strip()}"
     headers["Authorization"] = auth
+    
+    current_time = datetime.datetime.now()
+    formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S.%f")
+
+    headers["datetime"] = formatted_time
+    
     response = requests.post(url, headers=headers)
     if response.status_code == 200:
         print("签到成功")
