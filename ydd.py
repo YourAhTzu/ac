4b8eeb5a599a7a0263b2df883138d2e6f3ca3534
阿慈 <136092673+YourAhTzu@users.noreply.github.com>
2023-12-13 00:43:41 +0800

    更新 ydd.py

--- ydd.py
+++ ydd.py
@@ -1,24 +1,42 @@
-#阅多多 一天一次手动提
+'''
+不拉人玩不了
+'''
 import os
 import requests
 import time
 import re
-import json
+
+def check():
+    print("开始执行签到")
+    url = "http://ss88.aaaabb0.cfd//index.php?m=user&c=index&a=picker"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Linux; Android 11; V2068A Build/RP1A.200720.012; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/83.0.4103.106 Mobile Safari/537.36",
+        "X-Requested-With": "XMLHttpRequest",
+        "Referer": "http://ss88.aaaabb0.cfd//",
+        "Accept-Encoding": "gzip, deflate",
+        "Accept-Language": "zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7",
+        "Cookie": os.environ.get('ydd')
+    }
+
+    response = requests.get(url, headers=headers)
+    data = response.json()
+    info = data["info"]
+    print("签到结果：" + info)
 
 def obtain():
+    print("开始执行阅读")
     url = "http://ss88.aaaabb0.cfd/index.php?m=user&c=index&a=make_money_show"
     headers = {
         'User-Agent': 'Mozilla/5.0 (Linux; Android 11; V2068A Build/RP1A.200720.012; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/83.0.4103.106 Mobile Safari/537.36',
         'Cookie': os.environ.get('ydd')
     }
-    timestamp = int(time.time())
-    url_with_timestamp = f"{url}&time={timestamp}"
+
     for _ in range(10):
-        response = requests.get(url_with_timestamp, headers=headers)
+        response = requests.get(url, headers=headers)
         data_payload = response.text
         id_match = re.search(r'id\s*:\s*(\d+)', data_payload)
         if id_match:
-            read(id_match.group(1))  # 将阅读ID作为参数传递给read函数
+            read(id_match.group(1))
         time.sleep(5)
 
 def read(id):
@@ -31,23 +49,24 @@ def read(id):
         'User-Agent': 'Mozilla/5.0 (Linux; Android 11; V2068A Build/RP1A.200720.012; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/83.0.4103.106 Mobile Safari/537.36',
         'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
         'Origin': 'http://ss88.aaaabb0.cfd',
-        'Referer': 'http://ss88.aaaabb0.cfd/index.php?m=user&c=index&a=make_money_show&time=1702358114',
+        'Referer': 'http://ss88.aaaabb0.cfd/index.php?m=user&c=index&a=make_money_show',
         'Accept-Encoding': 'gzip, deflate',
         'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
         'Cookie': os.environ.get('ydd')
     }
 
     data = {
-        'id': id  # 使用传入的阅读ID作为参数
+        'id': id 
     }
 
     response = requests.post(url, headers=headers, data=data)
-    response_json = json.loads(response.text)
+    response_text = response.text
+    pattern = r'<info>(.*?)</info>'
+    match = re.search(pattern, response_text)
 
-    info = response_json.get('info')
-    if info:
+    if match:
+        info = match.group(1)
         print(info)
-    else:
-        print("Failed to get reading earnings information")
 
+check()
 obtain()
