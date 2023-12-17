8d5815e8e4dce1b90ad4a1fdac4afe24f278c7f5
阿慈 <136092673+YourAhTzu@users.noreply.github.com>
2023-12-18 00:47:45 +0800

    更新 btdl.py

--- btdl.py
+++ btdl.py
@@ -10,35 +10,7 @@ import re
 cks = os.environ.get('btdl').split('@')
 
 for i, ck in enumerate(cks, start=1):
-    print(f"开始运行第{i}个账号任务")
-    
-    url = 'http://123.129.219.247:9898/user/person.html'
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Linux; U; Android 12; zh-cn; RMX3562 Build/SP1A.210812.016) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/90.0.4430.61 Mobile Safari/537.36 HeyTapBrowser/40.7.37.9',
-        'Referer': 'http://123.129.219.247:9898/mobile/login.html',
-        'Cookie': ck,
-    }
-    
-    response = requests.get(url, headers=headers)
-    html = response.text
-    
-    pattern_username = r'<div class="t2">(.*?)<!--'
-    match_username = re.search(pattern_username, html)
-    
-    if match_username:
-        username = match_username.group(1)
-        print("{}, 当前余额{}".format(username, balance))
-    else:
-        print("未能获取用户名")
-    
-    pattern_balance = r'<div class="t3">(.*?)</div>'
-    match_balance = re.search(pattern_balance, html)
-    
-    if match_balance:
-        balance = match_balance.group(1)
-    else:
-        print("未能获取信息")
-    
+    print(f"开始运行第{i}个账号任务")   
     url = 'http://123.129.219.247:9898/mobile/form.html'
     headers = {
         'User-Agent': 'Mozilla/5.0 (Linux; Android 12; RMX3562 Build/SP1A.210812.016; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/97.0.4692.98 Mobile Safari/537.36',
