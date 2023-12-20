fe84a9c7aa03e081c5438612105518a7c1ac671b
阿慈 <136092673+YourAhTzu@users.noreply.github.com>
2023-12-20 14:34:46 +0800

    Add files via upload

---
+++ ApiRequest.py
@@ -0,0 +1,20 @@
+""""
+搬运作者:darkbfly
+"""
+import requests
+import urllib3
+
+import notify
+
+
+class ApiRequest:
+    def __init__(self):
+        urllib3.disable_warnings()
+        self.sec = requests.session()
+        self.sec.verify = False
+        self.sec.trust_env = False
+        self.sendmsg = ''
+        self.title = ''
+
+    def send(self):
+        notify.send(self.title, self.sendmsg)
