7716e7d0e385a64a9a3b6a5dd3497fa8df60e1a3
阿慈 <136092673+YourAhTzu@users.noreply.github.com>
2023-12-29 00:14:13 +0800

    更新 cfn.py

--- cfn.py
+++ cfn.py
@@ -5,24 +5,48 @@ import os
 
 token = os.environ.get('cfn')
 
-def lookVideo():
-    url = 'http://cfn.tuesjf.cn/apis/v1/lookVideo'
+def showIndexData():
+    url = f'http://cfn.tuesjf.cn/apis/v1/showIndexData?token={token}'
     headers = {
+        'Host': 'cfn.tuesjf.cn',
+        'Connection': 'keep-alive',
         'User-Agent': 'Mozilla/5.0 (Linux; Android 12; RMX3562 Build/SP1A.210812.016; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/97.0.4692.98 Mobile Safari/537.36  XiaoMi/MiuiBrowser/10.8.1 LT-APP/45/105/YM-RT/',
-        'Content-Type': 'application/x-www-form-urlencoded',
+        'content-type': 'application/x-www-form-urlencoded',
+        'Accept': '*/*',
+        'Origin': 'http://m.cfn.tuesjf.cn',
+        'X-Requested-With': 'com.hjtq',
         'Referer': 'http://m.cfn.tuesjf.cn/',
         'Accept-Encoding': 'gzip, deflate',
-        'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7'
+        'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
     }
-    data = {
-        'look_type': '1',
-        'token': token
-    }
-    response = requests.post(url, headers=headers, data=data)
-    result = response.json()
-    msg = result['msg']
-    print(f"观看结果:{msg}")
-    time.sleep(15)
+    response = requests.get(url, headers=headers)
+    data = response.json()
+    auto_save = data.get('data', {}).get('auto_save')
+    return auto_save
+
+def lookVideo():
+    while True:
+        auto_save = showIndexData()
+        if auto_save < 10:
+            url = 'http://cfn.tuesjf.cn/apis/v1/lookVideo'
+            headers = {
+                'User-Agent': 'Mozilla/5.0 (Linux; Android 12; RMX3562 Build/SP1A.210812.016; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/97.0.4692.98 Mobile Safari/537.36  XiaoMi/MiuiBrowser/10.8.1 LT-APP/45/105/YM-RT/',
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Referer': 'http://m.cfn.tuesjf.cn/',
+                'Accept-Encoding': 'gzip, deflate',
+                'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7'
+            }
+            data = {
+                'look_type': '1',
+                'token': token
+            }
+            response = requests.post(url, headers=headers, data=data)
+            result = response.json()
+            msg = result['msg']
+            print(f"观看结果:{msg}")
+            time.sleep(15)
+        else:
+            break
 
 def receiveVideo():
     print(">>>>>开始领取收益<<<<<")
@@ -69,7 +93,6 @@ def cachSave():
         print("已提现")
 
 if __name__ == "__main__":
-    while True:
-        lookVideo()
-        receiveVideo()
-        cachSave()
+    receiveVideo()
+    lookVideo()
+    cachSave()
