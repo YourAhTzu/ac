d3fc2408fe76f637c7f537ec9360743788148905
阿慈 <136092673+YourAhTzu@users.noreply.github.com>
2024-01-09 21:00:13 +0800

    Update cfn.py

--- cfn.py
+++ cfn.py
@@ -28,7 +28,7 @@ def showIndexData():
 def lookVideo():
     while True:
         auto_save = showIndexData()
-        if auto_save < 15:
+        if auto_save < 35:
             url = 'http://cfn.tuesjf.cn/apis/v1/lookVideo'
             headers = {
                 'User-Agent': 'Mozilla/5.0 (Linux; Android 12; RMX3562 Build/SP1A.210812.016; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/97.0.4692.98 Mobile Safari/537.36  XiaoMi/MiuiBrowser/10.8.1 LT-APP/45/105/YM-RT/',
