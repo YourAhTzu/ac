c29cd54a429b8fb8fd8e4085e50c5b22da18226c
阿慈 <136092673+YourAhTzu@users.noreply.github.com>
2024-01-10 07:56:58 +0800

    Add files via upload

--- mhdy.py
+++ mhdy.py
@@ -1,7 +1,7 @@
 '''
 new Env('梦幻岛屿');
 BY:YourAhTzu
-日期:1.2 22:21 (删除购买月卡更新月卡金币领取)3号可能上限金币交易
+日期:1.10 7:53 (修复多号报错)
 注册链接:http://mh.youwanzz.com/#/pages/reg?id=a413
 格式:账号&密码
 去主页绑定zfb在运行脚本需要签到十天
@@ -9,7 +9,8 @@ BY:YourAhTzu
 import os
 import requests
 import json
-
+import random
+import time
 def login(tel, pwd):
     print(">>>>>开始登录账号<<<<<")
     url = "http://mhapi.youwanzz.com:4005/index/Login"
@@ -35,10 +36,14 @@ def login(tel, pwd):
     }
     response = requests.post(url, headers=headers, data=data)
     data = response.json()
-    token = data["token"]
-    uid = data["id"]
-    print(data["msg"])
-    return token, uid
+    if "token" in data:
+        token = data["token"]
+        uid = data["id"]
+        print(data["msg"])
+        return token, uid
+    else:
+        print("登录失败:", data["msg"])
+        return None, None
 
 def qian_dao(token, uid):
     print(">>>>>开始执行签到<<<<<")
@@ -117,13 +122,28 @@ def GetHome(token, uid):
     jinbi = data["jinbi"]
     hongbao = data["hongbao"]
     print("当前金币:", jinbi,"当前红包:", hongbao)
+
 if __name__ == '__main__':
     mhdy = os.environ.get('mhdy')
     if mhdy:
-        tel, pwd = mhdy.split('&')
-        token, uid = login(tel, pwd)
-        qian_dao(token, uid)
-        LingRenWu(token, uid)
-        GetHome(token, uid)
+        account_list = mhdy.split('@')
+        for account in account_list:
+            tel, pwd = account.split('&')
+            token, uid = login(tel, pwd)
+            
+            if token is not None and uid is not None:
+                login_delay = random.randint(15, 25)
+                time.sleep(login_delay)
+                qian_dao(token, uid)
+                LingRenWu(token, uid)
+                GetHome(token, uid)
+                task_delay = random.randint(5, 10)
+                print(f"等待{task_delay}秒后执行下一个任务")
+                time.sleep(task_delay)
+
+                print("-------------------------------------")
+            else:
+                print("账号登录失败，请检查账号密码是否正确")
+                print("-------------------------------------")
     else:
         print("请设置环境变量 mhdy")
