59cfd589e531f6bcbd171b36c3c794aa70e3ea1f
阿慈 <136092673+YourAhTzu@users.noreply.github.com>
2024-01-23 09:21:51 +0800

    Add files via upload
    修改逻辑和错误

--- ddyx.py
+++ ddyx.py
@@ -3,14 +3,13 @@ cron: 0 8 * * *
 new Env('点点优选');
 微信捉包new.zzpt.top中的userid
 青龙变量 export ddyx="userid@userid" 多账号@隔开
-只做了抽其他再说
+基本任务已完成
 """
 import os
 import requests
 import time
 import random
 def sign(token):
-    print("领取抽奖机会")
     url = "https://new.zzpt.top/mini/task/free"
     headers = {
         "Host": "new.zzpt.top",
@@ -23,7 +22,6 @@ def sign(token):
         "userid": token,
         "Referer": "https://servicewechat.com/wxda7b83cfd34c7fdf/4/page-frame.html"
     }
-
     response = requests.post(url, headers=headers)
     response_data = response.json()
     code = response_data["code"]
@@ -32,9 +30,7 @@ def sign(token):
         print(f"签到结果:{msg}")
     else:
         print("已签到")
-
 def lottery(token):
-    print("开始执行抽奖")
     url = "https://new.zzpt.top/mini/jigsaw/get"
     headers = {
         "Host": "new.zzpt.top",
@@ -47,19 +43,16 @@ def lottery(token):
         "userid": token,
         "Referer": "https://servicewechat.com/wxda7b83cfd34c7fdf/4/page-frame.html"
     }
-
     data = {
         "messageStatus": "",
         "userToken": "",
         "accessType": "1"
     }
-
     response = requests.post(url, headers=headers, data=data)
     response_data = response.json()
     msg = response_data["msg"]
     print(f"抽奖结果:{msg}")
 def Exchange(token):
-    print("龙卡兑换余额")
     url = "https://new.zzpt.top/mini/jigsaw/exchangePriceByCard/87"
     headers = {
         "Host": "new.zzpt.top",
@@ -72,7 +65,6 @@ def Exchange(token):
         "userid": token,
         "Referer": "https://servicewechat.com/wxda7b83cfd34c7fdf/4/page-frame.html"
     }
-
     response = requests.post(url, headers=headers)
     response_data = response.json()
     code = response_data["code"]
@@ -85,20 +77,16 @@ if __name__ == "__main__":
     userid = os.environ.get("ddyx")
     if userid:
         tokens_list = userid.split("@")
-        for token in tokens_list:
+        for i, token in enumerate(tokens_list):
+            print(f">>>>>开始执行第 {i+1} 号任务<<<<<")  
+            print(">>>>>领取抽奖机会<<<<<")
             sign(token)
+            print(">>>>>龙卡兑换余额<<<<<")
             Exchange(token)
+            print(">>>>>开始执行抽奖<<<<<")
             for _ in range(8):
                 lottery(token)
                 time.sleep(random.randint(1, 5))
-    else:
-        print("未找到环境变量ddyx")if __name__ == "__main__":
-    userids = os.getenv("ddyx") 
-    if userids:
-        userids_list = userids.split("@") 
-        for i in range(8):
-            for userid in userids_list:
-                sign(userid)
-                time.sleep(5 + 5 * random.random())
+            print("========================================")  
     else:
         print("未找到环境变量ddyx")
