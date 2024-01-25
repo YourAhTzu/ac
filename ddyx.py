babb2f36683b3b5ee1fc8596f2ccf9db7b314ade
阿慈 <136092673+YourAhTzu@users.noreply.github.com>
2024-01-25 16:54:10 +0800

    Add files via upload

--- ddyx.py
+++ ddyx.py
@@ -1,10 +1,9 @@
 """
 cron: 0 8 * * *
 new Env('点点优选');
-入口:http://wxzdy.qrurl.net/wxcode/240124/1fe1dab43d3d0b4af3564c075b93465b.jpeg
 微信捉包new.zzpt.top中的userid
 青龙变量 export ddyx="userid@userid" 多账号@隔开
-邀请后期在做
+默认助力作者赞
 """
 import os
 import requests
@@ -31,6 +30,25 @@ def sign(token):
         print(f"签到结果:{msg}")
     else:
         print("已签到")
+def unauthorized(token):
+    url = "https://new.zzpt.top/mini/unauthorized/info/958893"
+    headers = {
+        "Host": "new.zzpt.top",
+        "Connection": "keep-alive",
+        "Content-Length": "0",
+        "charset": "utf-8",
+        "User-Agent": "Mozilla/5.0 (Linux; Android 12; RMX3562 Build/SP1A.210812.016; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/116.0.0.0 Mobile Safari/537.36 XWEB/1160049 MMWEBSDK/20231105 MMWEBID/2307 MicroMessenger/8.0.44.2502(0x28002C51) WeChat/arm64 Weixin NetType/WIFI Language/zh_CN ABI/arm64 MiniProgramEnv/android",
+        "content-type": "application/x-www-form-urlencoded",
+        "Accept-Encoding": "gzip,compress,br,deflate",
+        "userid": token,
+        "Referer": "https://servicewechat.com/wxda7b83cfd34c7fdf/4/page-frame.html"
+    }
+    response = requests.post(url, headers=headers)
+    response_data = response.json()
+    code = response_data["code"]
+    if code == 0:
+        msg = response_data["msg"]
+        print(f"助力结果:{msg}")        
 def video(token):
     url = "https://new.zzpt.top/mini/task/video"
     headers = {
@@ -92,7 +110,8 @@ def exchange(token):
     code = response_data["code"]
     if code == 0:
         msg = response_data["msg"]
-        print(f"兑换结果:{msg}")
+        price = response_data["data"]["price"]
+        print(f"兑换结果:{msg}, 获得余额:{price}")
     else:
         print("未达到要求兑换失败")
 def Exchange(token):
@@ -113,7 +132,8 @@ def Exchange(token):
     code = response_data["code"]
     if code == 0:
         msg = response_data["msg"]
-        print(f"兑换结果:{msg}")
+        price = response_data["data"]["price"]
+        print(f"兑换结果:{msg}, 获得余额:{price}")
     else:
         print("未达到要求兑换失败")
 if __name__ == "__main__":
@@ -121,18 +141,20 @@ if __name__ == "__main__":
     if userid:
         tokens_list = userid.split("@")
         for i, token in enumerate(tokens_list):
-            print(f">>>>>开始执行第{i+1}个号任务<<<<<")  
-            print(">>>>>开始每日登录<<<<<")
+            print(f">>>>>开始执行第{i+1}个账号任务<<<<<")  
+            print(">>>>>领取抽奖机会<<<<<")
             sign(token)
-            print(">>>>>开始激励广告<<<<<")    
+            print(">>>>>助力作者<<<<<")
+            unauthorized(token)
+            print(">>>>>开始观看广告<<<<<")    
             for _ in range(2):
                 video(token)
                 time.sleep(random.randint(15, 30))
-            print(">>>>>开始执行抽龙卡<<<<<")
+            print(">>>>>开始执行抽奖<<<<<")
             for _ in range(4):
                 lottery(token)
-                time.sleep(random.randint(5, 10))
-            print(">>>>>集卡兑换余额<<<<<")
+                time.sleep(random.randint(1, 10))
+            print(">>>>>福利红包<<<<<")
             exchange(token)
             print(">>>>>龙卡兑换余额<<<<<")
             Exchange(token)
