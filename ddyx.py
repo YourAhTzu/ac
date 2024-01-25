209f8629c6851199bc6bb7a8af564fcf38f0ac35
阿慈 <136092673+YourAhTzu@users.noreply.github.com>
2024-01-25 23:50:10 +0800

    Update ddyx.py

--- ddyx.py
+++ ddyx.py
@@ -2,13 +2,37 @@
 cron: 0 8 * * *
 new Env('点点优选');
 微信捉包new.zzpt.top中的userid
-青龙变量 export ddyx="userid@userid" 多账号@隔开
-默认助力作者赞
+青龙变量 export ddyx="userid@userid" 多账号@隔开  58行改助力码进行账号助力默认助力作者 (更新频繁)
 """
 import os
 import requests
 import time
 import random
+def info(token):
+    url = "https://new.zzpt.top/mini/user/userInfo"
+    headers = {
+        "Host": "new.zzpt.top",
+        "Connection": "keep-alive",
+        "Content-Length": "0",
+        "charset": "utf-8",
+        "User-Agent": "Mozilla/5.0 (Linux; Android 12; RMX3562 Build/SP1A.210812.016; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/116.0.0.0 Mobile Safari/537.36 XWEB/1160055 MMWEBSDK/20231105 MMWEBID/2307 MicroMessenger/8.0.44.2502(0x28002C51) WeChat/arm64 Weixin NetType/WIFI Language/zh_CN ABI/arm64 MiniProgramEnv/android",
+        "content-type": "application/x-www-form-urlencoded",
+        "Accept-Encoding": "gzip,compress,br,deflate",
+        "userid": token,
+        "Referer": "https://servicewechat.com/wxda7b83cfd34c7fdf/6/page-frame.html"
+    }
+    response = requests.post(url, headers=headers)
+    data = response.json()
+    userName = data["data"]["userName"]
+    userToken = data["data"]["userToken"]
+    totalPrice = data["data"]["totalPrice"]
+    newYearPrice = data["data"]["newYearPrice"]
+    dragonGold = data["data"]["dragonGold"]
+    print(f"用户:{userName}")
+    print(f"助力码:{userToken}")
+    print(f"钱包:{totalPrice}")
+    print(f"龙卡:{newYearPrice}")
+    print(f"龙币:{dragonGold}")
 def sign(token):
     url = "https://new.zzpt.top/mini/task/free"
     headers = {
@@ -158,6 +182,8 @@ if __name__ == "__main__":
             exchange(token)
             print(">>>>>龙卡兑换余额<<<<<")
             Exchange(token)
+            print(">>>>>用户查询<<<<<")
+            info(token)
             print("========================================")  
     else:
         print("未找到环境变量ddyx")
