9d324ad7e73de01b58959f99867b89c5ffd2b6ca
阿慈 <136092673+YourAhTzu@users.noreply.github.com>
2024-11-09 18:30:24 +0800

    Update xmyd.py

--- xmyd.py
+++ xmyd.py
@@ -4,24 +4,30 @@ PS:API百度搜索的所有侵权请联系@sylm1删除
 '''
 import requests
 import os
+import random
+BS = True
 class AC:
-    def __init__(self, account, password, step):
+    def __init__(self, account, password, min_step, max_step=None):
         self.Account = account
         self.Password = password
-        self.Step = step
+        self.MinStep = min_step
+        self.MaxStep = max_step if max_step else min_step  
         self.headers = {
             'User-Agent': "Mozilla/5.0 (Linux; Android 12; RMX3562 Build/SP1A.210812.016; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/122.0.6261.120 Mobile Safari/537.36 XWEB/1220133 MMWEBSDK/20240404 MMWEBID/2307 MicroMessenger/8.0.49.2600(0x28003133) WeChat/arm64 Weixin NetType/WIFI Language/zh_CN ABI/arm64 MiniProgramEnv/android"
         }
     def Brushstep(self):
-        url = f"https://api.yyink.cn/api/mi?user={self.Account}&password={self.Password}&step={self.Step}"
+        if BS:
+            step = random.randint(self.MinStep, self.MaxStep)  数
+        else:
+            step = self.MinStep
+        url = f"https://api.yyink.cn/api/mi?user={self.Account}&password={self.Password}&step={step}"
         response = requests.get(url, headers=self.headers)
         data = response.json()
         if data['code'] == 200:
-            userNickname = data['msg']
-            print(f"账户 {self.Account} 刷步成功，用户昵称：{userNickname}")
+            print(f"账户{self.Account}刷步成功，步数：{step}")
         else:
             msg = data["msg"]
-            print(f"账户 {self.Account} 刷步失败：{msg}")
+            print(f"账户{self.Account}刷步失败：{msg}")
 if __name__ == "__main__":
     xmyd = os.environ.get('xmyd')
     if not xmyd:
@@ -30,8 +36,12 @@ if __name__ == "__main__":
         xmyd_list = xmyd.split('#')
         for xmyd_item in xmyd_list:
             try:
-                account, password, step = xmyd_item.split('&')
-                ac = AC(account, password, step)
+                if BS:
+                    account, password, min_step, max_step = xmyd_item.split('&')
+                    ac = AC(account, password, int(min_step), int(max_step))
+                else:
+                    account, password, step = xmyd_item.split('&')
+                    ac = AC(account, password, int(step))
                 ac.Brushstep()
             except ValueError:
-                print(f"请按照'账号&密码&需要刷的步数'的形式多号#隔开")
+                print(f"请按照'账号&密码&最小步数&最大步数'的形式多号#隔开，或者当BS为False时，按照'账号&密码&步数'的形式")
